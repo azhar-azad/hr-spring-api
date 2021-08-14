cf6f933a9dd142c1f13d51067a93b13060bf7d12
azhar-azad <azhar.u.azad@gmail.com>
2021-08-14 23:51:24 +0600

    db conn configured

---
+++ src/main/java/com/azad/hrspringapi/HrSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.azad.hrspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrSpringApiApplication.class, args);
+	}
+
+}
